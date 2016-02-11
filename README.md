2c2da0f7034f941fd681a0544ce156ae720e713b
Ryan Pitts <ryan.a.pitts@gmail.com>
2016-02-11 15:07:00 -0800

    initial

---
+++ README.md
@@ -0,0 +1,154 @@
+### What this script does
+This script takes data from a Google spreadsheet, converts it to a JSON array
+where each object has keys corresponding to the spreadsheet column names, then
+then stores that JSON and/or automatically commits it to a GitHub repo.
+
+### Use cases
+It's a common pattern to use Google spreadsheets as an ad-hoc CMS, then need
+to pull that data into JSON to power a web app. It's also a common pattern
+to use GitHub Pages to serve up a simple web app. This script provides a little
+glue between these two patterns, taking advantage of GitHub Pages' automatic
+rebuild when a new commit is pushed to your repository.
+
+A version of this script provides data to the [SRCCON schedule app](https://github.com/OpenNews/srccon-schedule);
+we set up a Slackbot trigger to fire it off via chat command, pushing a new
+`sessions.json` to the schedule repo and automatically updating the app
+for all attendees.
+
+It can also be run on a cron; that's how we kept data updated in the
+[2015 MozFest schedule](https://github.com/mozilla/mozfest-schedule-app).
+
+### How it works
+There are four primary methods chained together to perform these task(s).
+
+* `fetch_data()`: uses Oauth2 credentials to authenticate with Google
+    and copy data from a spreadsheet into a Python dict.
+  
+* `transform_data()`: passes each item in the Python dict through a filter,
+    providing an opportunity to validate, transform, remove fields, etc.
+  
+* `make_json()`: converts the Python dict into a JSON array, and provides
+    an opportunity to store a local copy of the JSON (useful for testing).
+  
+* `commit_json()`: authenticates with GitHub and commits a JSON file
+    to the identified repository if data has changed since last update.
+
+Running `update_data()` will execute these four methods in succession.
+It can also be run from the command line: `python update_data.py`
+
+### Requirements
+To install the requirements for this script, create a Python virtual
+environment using `virtualenv` and `virtualenvwrapper` (see [this guide](http://www.silverwareconsulting.com/index.cfm/2012/7/24/Getting-Started-with-virtualenv-and-virtualenvwrapper-in-Python)
+if you're unfamiliar). Then `pip install -r requirements.txt`.
+
+### Authentication
+For authentication to work, you must generate Google and GitHub credentials.
+These should be stored as environment variables, and *not* committed to the
+repository along with this.
+
+* **GitHub:** Follow these instructions to create a person API token:
+    https://github.com/blog/1509-personal-api-tokens
+    
+    You should generate the token from an account that has write access
+    to the repository where you want to store the JSON. It should be stored
+    in an environment variable called `GITHUB_TOKEN`.
+    
+* **Google:** Follow these instructions to create *a service account*:
+    https://developers.google.com/console/help/new/?hl=en_US#credentials-access-security-and-identity
+    
+    Creating a service account will generate a special client email address
+    and a new private key. The client email address should be stored in
+    an environment variable called `GOOGLE_API_CLIENT_EMAIL`. The private key
+    should be stored in an environment variable called `GOOGLE_API_PRIVATE_KEY`.
+
+### Basic project settings
+You should change the values in `GITHUB_CONFIG` according to your project.
+
+* **REPO_OWNER:** a string representing the GitHub username of the account
+    that owns the repository you want to commit to.
+    
+* **REPO_NAME:** a string representing the name of the repository to commit to.
+
+* **TARGET_FILE:** a string representing the name of the file you want to create
+     or update in the GitHub repository. This can include path information,
+     e.g. 'sessions.json', or '_data/sessions.json'
+
+* **TARGET_BRANCHES:** a list representing the branch(es) of the repository you
+    want to commit to, e.g. ['gh-pages',] or ['gh-pages','master']
+
+You should also change these values according to your project.
+
+* **GOOGLE_SPREADSHEET_KEY:** a string representing the unique ID of the Google
+    spreadsheet storing your data. This can be stored as an environment
+    variable called GOOGLE_SPREADSHEET_KEY for extra security, or can simply
+    be stored as a string in this script.
+
+* **FETCH_MULTIPLE_WORKSHEETS:** set to `True` if `GOOGLE_SPREADSHEET_KEY` points
+    to a document with data in multiple worksheets. The import will retrieve data
+    from all worksheets and compile into a single JSON file. _NOTE:_ The import
+    will not perform any validation or normalization on column names, so if
+    worksheets have varying column names, the resulting JSON objects will have
+    varied key names as well.
+
+* **WORKSHEETS_TO_SKIP:** if `FETCH_MULTIPLE_WORKSHEETS` is set to `True`, you
+    may define a list of worksheet names to ignore data from.
+    e.g. ['Template', '(backup data)']
+
+* **MAKE_LOCAL_JSON:** should likely be set to `False` in production, but can
+    be set to `True` for  testing. If set to `True`, the `make_json()` method
+    will create a local file containing the JSON for you to inspect.
+
+* **COMMIT_JSON_TO_GITHUB:** should be set to `True` in production. If set
+    to `True`, the `commit_json()` method will create or update a JSON file
+    in the GitHub repository you specify. Can be set to `False` for testing,
+    which will authenticate with GitHub but not create or update any files.
+
+### Data transformations
+In many cases, you'll want to apply validation, formatting or transformations
+to your data before turning it into JSON, or even derive new keys/values based
+on data from specific columns. The `transform_data` method runs every row
+of data through an internal `_transform_response_item` function, providing
+a place for code that affects each object destined for your JSON. By default,
+`_transform_response_item` ensures that each value is translated to a unicode
+string, and gives you access to a `_transformed_item` to manipulate.
+
+So if you wanted to, for example, create `day` and `time` values in your JSON
+based on concatenated values in a spreadsheet column called `when`, you could
+add the code below to `_transform_response_item`:
+
+```
+if 'when' in _transformed_item:
+    # split values like 'Saturday 7pm' into `day` and `time`
+    when = _transformed_item.pop('when', '').split(' ')
+    _transformed_item['day'] = when[0]
+    _transformed_item['time'] = when[1]
+```
+
+You may also set a `skip` flag during the `_transform_response_item` loop
+to eliminate a row from the JSON output. For example, if you want to ignore
+rows that do not have a numeric value in an `id` column in your spreadsheet:
+
+```
+if 'id' in _transformed_item:
+    # remove rows without a numeric `id`
+    try:
+        int(_transformed_item['id'])
+    except:
+        skip = True
+```
+
+### Usage notes
+The `fetch_data` method pulls data from the spreadsheet identified in
+`GOOGLE_SPREADSHEET_KEY`. If you have data spread across multiple worksheets,
+you can set `FETCH_MULTIPLE_WORKSHEETS` to `True`, and optionally provide
+a list of sheet names to skip in `WORKSHEETS_TO_SKIP`.
+
+It's more likely that all your data will come from a single worksheet; in this
+case, the script defaults to fetching from the first one in the spreadsheet.
+
+    datasheet = spreadsheet.get_worksheet(0)
+    
+If data is *not* in the first worksheet, you can provide a name value in
+`GOOGLE_SPREADSHEET_SHEETNAME` to identify the worksheet by title:
+
+    datasheet = spreadsheet.worksheet(GOOGLE_SPREADSHEET_SHEETNAME)
---
+++ update_data.py
@@ -0,0 +1,257 @@
+import argparse, os, sys, traceback
+import github3
+import gspread
+import io
+import json
+import logging
+import os
+import requests
+from datetime import datetime
+from logging.config import dictConfig
+from oauth2client.client import SignedJwtAssertionCredentials
+
+GITHUB_CONFIG = {
+    'TOKEN': os.environ['GITHUB_TOKEN'],
+    'REPO_OWNER': 'your_username',# change this
+    'REPO_NAME': 'your_reponame',# change this
+    'TARGET_FILE': 'data.json',
+    'TARGET_BRANCHES': ['gh-pages',],# choose one or more branches
+}
+
+GOOGLE_API_CONFIG = {
+    'CLIENT_EMAIL': os.environ['GOOGLE_API_CLIENT_EMAIL'],
+    'PRIVATE_KEY': os.environ['GOOGLE_API_PRIVATE_KEY'].decode('unicode_escape'),
+    'SCOPE': ['https://spreadsheets.google.com/feeds']
+}
+
+# the unique ID of the spreadsheet with your data can be stored
+# as an environment variable or simply added here as a string
+GOOGLE_SPREADSHEET_KEY = 'your_spreadsheet_key'
+#GOOGLE_SPREADSHEET_KEY = os.environ['GOOGLE_SPREADSHEET_KEY']
+
+# pull data from a named worksheet, or leave blank to assume first worksheet
+GOOGLE_SPREADSHEET_SHEETNAME = ''
+
+# if data is spread across multiple worksheets, set to True
+FETCH_MULTIPLE_WORKSHEETS = False
+
+# if fetching multiple worksheets, name sheets to skip here
+# EXAMPLE: WORKSHEETS_TO_SKIP = ['Sheet1', 'Sheet4',]
+WORKSHEETS_TO_SKIP = []
+
+# set to True to store local version of JSON
+MAKE_LOCAL_JSON = True
+
+# set to False for dry runs
+COMMIT_JSON_TO_GITHUB = False
+
+
+def authenticate_with_google():
+    '''
+    Connect to Google Spreadsheet with gspread library.
+    '''
+    credentials = SignedJwtAssertionCredentials(
+        GOOGLE_API_CONFIG['CLIENT_EMAIL'], GOOGLE_API_CONFIG['PRIVATE_KEY'], GOOGLE_API_CONFIG['SCOPE']
+    )
+    google_api_conn = gspread.authorize(credentials)
+    
+    return google_api_conn
+    
+def open_google_spreadsheet():
+    '''
+    Authenticate and return spreadsheet by `GOOGLE_SPREADSHEET_KEY`.
+    '''
+    google_api_conn = authenticate_with_google()
+    spreadsheet = google_api_conn.open_by_key(GOOGLE_SPREADSHEET_KEY)
+    
+    return spreadsheet
+
+def fetch_data(multiple_sheets=False, worksheets_to_skip=[]):
+    spreadsheet = open_google_spreadsheet()
+
+    if not multiple_sheets:
+        # Return data from named worksheet if specified ...
+        if GOOGLE_SPREADSHEET_SHEETNAME:
+            worksheet = spreadsheet.worksheet(GOOGLE_SPREADSHEET_SHEETNAME)
+        # .. otherwise return data from the first worksheet
+        else:
+            worksheet = spreadsheet.get_worksheet(0)
+
+        data = worksheet.get_all_records(empty2zero=False)
+
+    else:
+        # Return data from all worksheets in Google spreadsheet, optionally
+        # skipping sheets identified by title in `WORKSHEETS_TO_SKIP`
+        data = []
+        worksheet_list = [
+            sheet for sheet in spreadsheet.worksheets() if sheet.title not in WORKSHEETS_TO_SKIP
+        ]
+
+        for worksheet in worksheet_list:
+            worksheet.title
+            data.extend(worksheet.get_all_records(empty2zero=False))
+
+    return data
+
+def transform_data(data):
+    '''
+    Transforms data and filters/validates individual spreadsheet rows
+    for fields we want in the JSON output. Currently, this:
+    
+    * ensures that all variables going into the JSON are strings
+    
+    Additional filters should be added to _transform_response_item.
+    '''
+    def _transform_response_item(item, skip=False):
+        # make sure vars are strings
+        _transformed_item = {k: unicode(v) for k, v in item.iteritems() if k}
+        
+        # EXAMPLE: get rid of data from column `rowNumber`
+        # if 'rowNumber' in _transformed_item:
+        #     del _transformed_item['rowNumber']
+        
+        # EXAMPLE: rename spreadsheet column `name` into JSON key `title`
+        # if 'name' in _transformed_item:
+        #     _transformed_item['title'] = _transformed_item.pop('name', '')
+        
+        # EXAMPLE: use `skip` flag to ignore rows without valid id
+        # if 'id' in _transformed_item:
+        #     try:
+        #         int(_transformed_item['id'])
+        #     except:
+        #         skip = True
+        
+        # if we've triggered the skip flag anywhere, drop this record
+        if skip:
+            _transformed_item = None
+            
+        return _transformed_item
+    
+    # pass spreadsheet rows through the transformer
+    transformed_data = filter(None, [_transform_response_item(item) for item in data])
+
+    return transformed_data
+
+def make_json(data, store_locally=False, filename=GITHUB_CONFIG['TARGET_FILE']):
+    '''
+    Turns data into nice JSON, and optionally stores to a local file.
+    '''
+    json_out = json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False)
+    
+    if store_locally:
+        with io.open(filename, 'w', encoding='utf8') as outfile:
+            outfile.write(unicode(json_out))
+
+    return json_out.encode('utf-8')
+
+def commit_json(data, target_config=GITHUB_CONFIG, commit=COMMIT_JSON_TO_GITHUB):
+    '''
+    Uses token to log into GitHub, then gets the appropriate repo based
+    on owner/name defined in GITHUB_CONFIG.
+    
+    Creates data file if it does not exist in the repo, otherwise updates
+    existing data file.
+    
+    If `COMMIT_JSON_TO_GITHUB` is False, this will operate in "dry run" mode,
+    authenticating against GitHub but not changing any files.
+    '''
+    
+    # authenticate with GitHub
+    gh = github3.login(token=target_config['TOKEN'])
+    
+    # get the right repo
+    repo = gh.repository(target_config['REPO_OWNER'], target_config['REPO_NAME'])
+    
+    for branch in target_config['TARGET_BRANCHES']:
+        # check to see whether data file exists
+        contents = repo.contents(
+            path=target_config['TARGET_FILE'],
+            ref=branch
+        )
+
+        if commit:
+            if not contents:
+                # create file that doesn't exist
+                repo.create_file(
+                    path=target_config['TARGET_FILE'],
+                    message='adding session data',
+                    content=data,
+                    branch=branch
+                )
+                logger.info('Created new data file in repo')
+            else:
+                # if data has changed, update existing file
+                if data.decode('utf-8') == contents.decoded.decode('utf-8'):
+                    logger.info('Data has not changed, no commit created')
+                else:
+                    repo.update_file(
+                        path=target_config['TARGET_FILE'],
+                        message='updating schedule data',
+                        content=data,
+                        sha=contents.sha,
+                        branch=branch
+                    )
+                    logger.info('Data updated, new commit to repo')
+                
+
+def update_data():
+    data = fetch_data(multiple_sheets=FETCH_MULTIPLE_WORKSHEETS, worksheets_to_skip=WORKSHEETS_TO_SKIP)
+    #print 'Fetched the data ...'
+
+    data = transform_data(data)
+    #print 'Prepped the data ...'
+
+    json_data = make_json(data, store_locally=MAKE_LOCAL_JSON)
+    #print 'Made some JSON!'
+
+    commit_json(json_data)
+    #print 'Sent the data to GitHub!'
+
+
+'''
+Set up logging.
+'''
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'log.txt',
+            'formatter': 'verbose'
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'schedule_loader': {
+            'handlers':['file','console'],
+            'propagate': False,
+            'level':'DEBUG',
+        }
+    }
+}
+dictConfig(LOGGING)
+logger = logging.getLogger('schedule_loader')
+
+
+if __name__ == "__main__":
+    try:
+        update_data()
+    except Exception, e:
+        sys.stderr.write('\n')
+        traceback.print_exc(file=sys.stderr)
+        sys.stderr.write('\n')
+        sys.exit(1)
